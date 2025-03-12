42e0e3a22b978f182c4bfaade5c27821e9f57844
aakash-dec7 <Aakash.dec7@gmail.com>
2025-03-12 08:54:13 +0530

    setup.py updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sentana",
+    version="1.0.0",
+    author="Aakash Singh",
+    author_email="aakash.dec7@gmail.com",
+    packages=find_packages(),
+)
